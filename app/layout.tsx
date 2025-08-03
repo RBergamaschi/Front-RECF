2b61f057a40bf098fbe83f8fa6b5002e5f317475
Nicolas Pinheiro <151929844+NickGomes1@users.noreply.github.com>
2025-08-03 15:19:00 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body>{children}</body>
     </html>
   );
